082f632dc4b7dab2e38cbfbdac8f13c9c839fa39
yifan <yifan.zhang.25fall@outlook.com>
2026-02-22 11:49:42 +0000

    Update base URL for new repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/london-fire/',
+  base: '/casa0028-assessment/',
   plugins: [
     react({
       babel: {
